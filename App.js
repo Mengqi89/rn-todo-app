722939980e21bba7e6b2ad3bc32976470a1b198d
Mengqi Wang <wmq516@gmail.com>
2020-03-09 16:30:03 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is going to be a todo list. </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
