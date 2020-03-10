fe6e114718bb5858b0c7214170b1b3ad83b2e69a
Mengqi Wang <wmq516@gmail.com>
2020-03-10 10:06:44 -0600

    turn App into class component and create a state

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is going to be a todo list. </Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Take out the trash', done: false },
+        { id: 1, title: 'Cook dinner', done: false }
+      ]
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text>This is going to be a todo list. </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
